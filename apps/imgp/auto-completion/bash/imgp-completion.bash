f27792d31881440918dcb4b3da2ee457ffa6489c
phantasea <phantasea@gmail.com>
2020-04-26 13:29:53 +0300

    apps/imgp remove submodule and add again

---
+++ apps/imgp/auto-completion/bash/imgp-completion.bash
@@ -0,0 +1,55 @@
+#
+# Bash completion definition for imgp.
+#
+# Author:
+#   Arun Prakash Jana <engineerarun@gmail.com>
+#
+
+_imgp () {
+    COMPREPLY=()
+    local IFS=$' \n'
+    local cur=$2 prev=$3
+    local -a opts opts_with_args
+    opts=(
+        -a --adapt
+        -c --convert
+        -d --dot
+        -e --eraseexif
+        -f --force
+        -h --help
+        -i --includeimgp
+        -k --keep
+        -m --mute
+        -n --enlarge
+        --nn
+        -o --rotate
+        -p --optimize
+        --pr --progressive
+        -q --quality
+        -r --recurse
+        -s --size
+        -w --overwrite
+        -x --res
+        -z --debug
+    )
+    opts_with_arg=(
+        -o --rotate
+        -q --quality
+        -s --size
+        -x --res
+    )
+
+    # Do not complete non option names
+    [[ $cur == -* ]] || return 1
+
+    # Do not complete when the previous arg is an option expecting an argument
+    for opt in "${opts_with_arg[@]}"; do
+        [[ $opt == $prev ]] && return 1
+    done
+
+    # Complete option names
+    COMPREPLY=( $(compgen -W "${opts[*]}" -- "$cur") )
+    return 0
+}
+
+complete -F _imgp imgp
