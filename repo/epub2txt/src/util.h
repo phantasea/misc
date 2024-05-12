0190c1716ebacfaa93dc32a98284afed7aa93bf5
phantasea <phantasea@gmail.com>
2024-05-12 13:52:30 +0300

    repo/epub2txt added

---
+++ repo/epub2txt/src/util.h
@@ -0,0 +1,15 @@
+/*============================================================================
+  epub2txt v2 
+  util.h
+  Copyright (c)2022 Marco Bonelli, Kevin Boone GPL v3.0
+============================================================================*/
+
+#pragma once
+
+#include "defs.h"
+
+int run_command (const char *const argv[], BOOL abort_on_error);
+
+/** Decode %xx in URL-type strings. The caller must free the resulting
+    string, which will be no longer than the input. */
+char *decode_url (const char *url);
