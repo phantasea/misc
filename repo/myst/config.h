70410e5325f2dc3a937abeae8511092c4944f725
phantasea <phantasea@gmail.com>
2024-12-21 11:04:27 +0200

    myst:config.h changed

--- repo/myst/config.h
+++ repo/myst/config.h
@@ -7,7 +7,7 @@
  */
 //static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
 //static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
-static char *font = "mono:pixelsize=36:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=43:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=25:antialias=true:autohint=true" };
 static int borderpx = 1;
 
